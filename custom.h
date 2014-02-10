ab0bfae87106d1337299a1f9bdfd6558b372e033
philliplab <philliplab@gmail.com>
2014-02-10 14:20:01 +0200

    Changes child mort, root finding, disease_states
    Adds parameters that the user can toggle to child mortlity, primarily
    for the purpose of testing the access of the mother parameters from the
    child.
    Replaces the root finding procedures with a simple inverted version of
    the function.
    Removes the infected agent_var and replaces it with the more general
    disease_status variable

--- custom.h
+++ custom.h
@@ -1,14 +1,6 @@
 #ifndef CustomH
 #define CustomH
 
-#include <boost/math/tools/roots.hpp>
-
-class InfMortSurvCurve {
-public:
-	double kappa;
-	double lambda;
-	double phi;
-	double operator() (const double x);
-};
+double InvWeibull(double y, double kappa, double lambda, double phi);
 
 #endif
