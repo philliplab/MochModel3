c0f4c96993b2b27a9bf9a51d9d349ea35f1740a5
philliplab <philliplab@gmail.com>
2014-02-11 14:42:58 +0200

    CLEANS tables; stop tracking unnecessary files

--- custom.h
+++
@@ -1,7 +0,0 @@
-#ifndef CustomH
-#define CustomH
-
-double InvWeibull(double y, double kappa, double lambda, double phi);
-double InvWeibull2(double y, double kappa, double lambda, double phi);
-
-#endif
